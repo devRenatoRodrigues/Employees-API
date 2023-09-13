b6c8cc73268dd243c63edd3e129c7173ae57d126
devRenatoRodrigues <rehrlz@hotmail.com>
2023-09-13 00:23:29 -0300

    feat: create employee controller

---
+++ src/controllers/Employee.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import mapStatusHTTP from "../utils/mapStatusHTTP.utils";
+import EmployeeService from "../services/Employee.service";
+
+
+export default class EmployeeController {
+    constructor(
+        private _employeeService = new EmployeeService(),
+    ) { }
+
+    async findAll(_req: Request, res: Response): Promise<Response> {
+        const serviceResponse = await this._employeeService.findAll()
+        return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    async findByPk(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params
+        const serviceResponse = await this._employeeService.findByPk(id)
+        return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    async update(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+        const data = req.body;
+        const serviceResponse = await this._employeeService.update(id, data)
+        return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    async create(req: Request, res: Response): Promise<Response> {
+        const data = req.body;
+        const serviceResponse = await this._employeeService.create(data)
+        return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    async delete(req: Request, res: Response): Promise<Response>  {
+        const { id } = req.params;
+        const serviceResponse = await this._employeeService.delete(id)
+        return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+
+    }
+}
